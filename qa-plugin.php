711f82aa613e7527f56ec05ef9e7121a8b65c7fa
Dmitry <slonoed@gmail.com>
2010-12-28 17:40:58 +0300

    Add some instructions in README and add qa-plugin.php
    Signed-off-by: Dmitry <slonoed@gmail.com>

---
+++ qa-plugin.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+	Question2Answer 1.3 (c) 2010, SLonoed
+
+	http://www.question2answer.org/
+
+	
+	File: qa-plugin/loginza-login/qa-loginza.php
+	Version: 1.3
+	Date: 2010-11-23 06:34:00 GMT
+	Description: Initiates Facebook login plugin
+
+
+
+*/
+
+/*
+	Plugin Name: Facebook Login
+	Plugin URI: 
+	Plugin Description: Allows users to log in via Facebook
+	Plugin Version: 1.0
+	Plugin Date: 2010-10-31
+	Plugin Author: Question2Answer
+	Plugin Author URI: http://www.question2answer.org/
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.3
+*/
+
+
+	if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+		header('Location: ../../');
+		exit;
+	}
+
+
+	qa_register_plugin_module('login', 'qa-loginza-login.php', 'qa_loginza_login', 'Loginza');
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
